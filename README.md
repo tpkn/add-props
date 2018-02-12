4cfa2978ab4c9b30ef082352eaf5b867b60436ec
tpkn <tpkn@users.noreply.github.com>
2018-02-12 23:08:35 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# add-props
+# Add Props
 Easiest way to add props and values to the object
+
+
+## Usage
+```javascript
+var myObject = {};
+
+
+// Just create properties structure
+addProps(myObject, 'click.url');
+
+/*{
+   "click": {
+      "url": {}
+   }
+}*/
+
+
+// Add value to the property
+addProps(myObject, 'events.tracker.pixel.url', 12346789);
+
+/*{
+   "events": {
+      "tracker": {
+         "pixel": {
+            "url": 12346789
+         }
+      }
+   }
+}*/
+
+
+// Create multiple properties and fill them with the same value
+addProps(myObject, ['stat.user_id', 'html.footer.block_id'], 'id123456789');
+
+/*{
+   "stat": {
+      "user_id": "id123456789"
+   },
+   "html": {
+      "footer": {
+         "block_id": "id123456789"
+      }
+   }
+}*/
+
+
+// Create multiple properties with own values
+addProps(myObject, ['video.duration', 'audio.sound.muted'], [120.459, true]);
+
+/*{
+   "video": {
+      "duration": 120.459
+   },
+   "audio": {
+      "sound": {
+         "muted": true
+      }
+   }
+}*/
+
+
+console.log(JSON.stringify(myObject, true, 3));
+```
+
---
+++ add-props.js
@@ -0,0 +1,52 @@
+/*!
+ * Add Props (v1.1.0.20180211), http://tpkn.me/
+ */
+
+function addProps(obj, props, values){
+   var check = function(p, level, list, val){
+      if(!Object.prototype.hasOwnProperty.call(level, p)){
+         level[p] = {};
+      }
+      if(typeof val !== 'undefined' && val != null && list.length == 0){
+         level[p] = val;
+      }
+   }
+
+   var loop = function(level, list, val){
+      if(list.length){
+         var p = list.shift();
+
+         // Check if it's regular_property_Name (accessible through dot). Skip further steps if it's not
+         if(!/(\W|^[\d\s]+$)/i.test(p)){
+            check(p, level, list, val);
+         }else{
+            return;
+         }
+
+         loop(level[p], list, val);
+      }
+   }
+
+   if(!Array.isArray(props) && typeof props !== 'string'){
+      return console.log('Property is not a string or array!');
+   }
+
+   if(Array.isArray(props)){
+      for(var i = 0, len = props.length; i < len; i++){
+         loop(obj, String(props[i]).split('.'), Array.isArray(values) ? values[i] : values);
+      }
+   }else{
+      loop(obj, String(props).split('.'), values);
+   }
+}
+
+
+if(typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+   module.exports = addProps;
+}else{
+   if(typeof define === 'function' && define.amd){
+      define([], function(){
+         return addProps;
+      });
+   }
+}
